c52fc742db876769fe1a5f86c2ab6e1dfa02ed79
Tailon <renewedT2016@gmail.com>
2020-03-13 15:56:33 -0800

    Update test to fix misunderstanding with directions

--- main.cpp
+++ main.cpp
@@ -11,43 +11,37 @@ TEST_CASE("Start of game -- score is 0 to 0")
 TEST_CASE("Teams scored 1 points")
 {
 	ScoreKeeper mark;
-	mark.scoreTeamB3();
+	mark.scoreTeamB1();
 	CHECK(mark.getScore() == "000:001");
-	mark.scoreTeamA3();
+	mark.scoreTeamA1();
 	CHECK(mark.getScore() == "001:001");
 	
-	mark.scoreTeamB3();
+	mark.scoreTeamB1();
 	CHECK(mark.getScore() == "001:002");
-	mark.scoreTeamA3();
+	mark.scoreTeamA1();
 	CHECK(mark.getScore() == "002:002");
 	
-	mark.scoreTeamA3();
+	mark.scoreTeamA1();
 	CHECK(mark.getScore() == "003:002");
-	mark.scoreTeamB3();
+	mark.scoreTeamB1();
 	CHECK(mark.getScore() == "003:003");
 }
 
 TEST_CASE("Teams scored 2 points")
 {
 	ScoreKeeper mark;
-	mark.scoreTeamB3();
-	mark.scoreTeamB3();
+	mark.scoreTeamB2();
 	CHECK(mark.getScore() == "000:002");
-	mark.scoreTeamA3();
-	mark.scoreTeamA3();
+	mark.scoreTeamA2();
 	CHECK(mark.getScore() == "002:002");
 	
-	mark.scoreTeamB3();
-	mark.scoreTeamB3();
+	mark.scoreTeamB2();
 	CHECK(mark.getScore() == "002:004");
-	mark.scoreTeamA3();
-	mark.scoreTeamA3();
+	mark.scoreTeamA2();
 	CHECK(mark.getScore() == "004:004");
 	
-	mark.scoreTeamA3();
-	mark.scoreTeamA3();
+	mark.scoreTeamA2();
 	CHECK(mark.getScore() == "006:004");
-	mark.scoreTeamB3();
-	mark.scoreTeamB3();
+	mark.scoreTeamB2();
 	CHECK(mark.getScore() == "006:006");
 }
